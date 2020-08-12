5ee9a9bb82d6e727fde3d77f1e7ed316012fdd08
Karol-Oleksy <karol.oleksy.98@gmail.com>
2020-08-12 13:28:44 +0200

    Edycja kodów, zmiana nazw dźwięków
    Praca nad funkcjami do zastępowania głosek

--- substitution.py
+++ substitution.py
@@ -1,36 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-Zamiana głosek (dyftongi, głoski niewyróżniające się)
+Zamiana głosek (dyftongi, głoski inaczej wymawiane)
 """
 
-def subst_unused(string):
-    '''
-    Zastępuje głoski, które nie wyróżniają się w tekscie
-    '''
-    string.replace('ó','u')
-    string.replace('b ','p ')
-    string.replace('b.', 'b.')
-    
-    
-    
-   
-    
-    
-    
-    return
-
-
-
+#należy najpierw wykonać zamianę dyftongów, potem uwzględnić różnice w wymowie
 
 def dipht_simpl(string):
+    '''
+    Zastępuje dyftongi pojedynczymi znakami
+    '''
     
     string.replace('ch','h')
-    string.replace('rz','ż')
-    string.replace('dz','C')
-    string.replace('dż','D')
-    string.replace('dź','Δ')
-    string.replace('sz','S')
+    string.replace('rz','ż')  #wziąć pod uwagę, że są słowa jak 'marznąć'
+    string.replace('dz','ζ')
+    string.replace('dż','δ')
+    string.replace('dź','∂')
+    string.replace('sz','σ')
     string.replace('cz','3')
    
     
     return    
+
+
+def subst_diff(string):
+    '''
+    Zastępuje w słowie głoski, które są wymawiane inaczej
+    ze względu na różnego rodzaju procesy fonetyczne
+    '''
+    voiced = ['b','d','ζ','∂','δ','g','w','z','ź','ż']
+    unvoiced = ['p','t','c','ć','3','k','f','s','ś','σ','h']
+    
+    #homofonia
+    string.replace('ó','u')
+    string.replace('au','ał')
+    
+    #ubezdźwięcznienie na końcu wyrazu
+    if string[len(string)-1] in voiced:
+        string = string[:len(string)-1] + unvoiced[voiced.index(string[len(string)-1])]
+    
+    #ubezdźwięcznienie przez upodobnienie
+    
+    
+    
+    
+   
+    
+    
+    
+    return string
+
+
+a = "siema, jestem karol. co ty na to?"
+
+b = a.split()
+print(b)
+
+for x in range(len(b)):
+   b[x] = b[x].strip('-!?()[]{}:;+=/\\&\'\".,')
+
+print(b)
--- t_division.py
+++ t_division.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Próbny skrypt do dzielenia wyrazu na sylaby
 """
@@ -7,11 +7,17 @@ import shortcuts
 import substitution
 
 vowels = ['a','ą','e','ę','i','o','u','y']
-consonants = ['b','c','ć','d','f','g','h','j','k','l','ł','m','n','ń','p','r','s','ś','t','w','z','ź','ż']
+consonants = ['b','c','ć','d','f','g','h','j','k','l','ł','m','n','ń','p','r','s','ś','t','w','z','ź','ż','ζ','∂','δ','3','σ']
 
 
 sentence = 'koronawirus to wirus'
 
+#przed podzieleniem na wyrazy trzeba poddać zdanie podstawieniom z pliku substitution.py
+#skrótowców też trzeba poszukać tutaj, przed podziałem na wyrazy,
+# bo trzeba dać do lowercase wyrazy zaczynające się wielką literą,
+# żeby dobrze dokonały się podstawienia
+
+#albo można po prostu działać od razu na słowach zamiast na całosci...
 
 words = sentence.split()
 
@@ -20,15 +26,23 @@ vowel_i = []
 
 for word in words:
     
+    #usunięcie znaków przestankowych i innych
+    word = word.strip('-!?()[]{}:;+=/\\&\'\".,')
+
+    #wymowa skrótowca
     if word.isupper():
-        syllables = shortcuts.spell(word)
-    
+        syls = shortcuts.spell(word)
     else:
-        for i in range(len(word)-1):
-            if word[i] in vowels:
-                vowel_i.append(i)
-        for i in range(len(vowel_i)-1):
-            if vowel_i[i]
+        word = word.lower()
+        
+    for i in range(len(word)-1):
+        if word[i] in vowels:
+            vowel_i.append(i)
+    for i in range(len(vowel_i)-1):
+        if vowel_i[i]:
+            
+            
+    syllables.append(syls) #inaczej, bo syls to lista
 
 print(syllables)
 
