fc3cd440c4bb7640f51c0d7cc3dc14dc0bf693c2
Karol-Oleksy <karol.oleksy.98@gmail.com>
2020-08-23 14:26:55 +0200

    Nagrywanie sylab

--- t_play.py
+++ t_play.py
@@ -10,13 +10,6 @@ from pydub import AudioSegment
 from pydub.playback import play
 from pydub.effects import normalize
 
-
-#ko = sa.WaveObject.from_wave_file('Dźwięki/ko.wav')
-#że = sa.WaveObject.from_wave_file('Dźwięki/że.wav')
-#ńe = sa.WaveObject.from_wave_file('Dźwięki/ńe.wav')
-#wi = sa.WaveObject.from_wave_file('Dźwięki/wi.wav')
-#rus = sa.WaveObject.from_wave_file('Dźwięki/rus.wav')
-
 word = main('Litwo! Ojczyzno moja! ty jesteś jak zdrowie: Ile cię trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po tobie. ')
 i=0
 for syllable in word:
